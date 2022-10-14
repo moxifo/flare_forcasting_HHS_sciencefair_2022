b28bef6eadb352c502f9631478b8628b9d3ca64e
azim <azim.ahmadzadeh@gmail.com>
2022-10-14 11:38:22 -0400

    Created a place for your vis methods.

---
+++ visualisation/visualize_data_stats.py
@@ -0,0 +1,8 @@
+# This script is a good place for your visualization methods.
+
+# TODO: for each visualization you should have one method. If there are
+#  snipets of code that would be repeated in each method, take them out and
+#  create separate methods for them as well. So, most of your code that is
+#  now in `main.py` should be moved here, but they all should go in methods.
+
+#  No snipet of code should wander around outside a method.
