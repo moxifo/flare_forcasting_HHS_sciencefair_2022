ad11c5f5fb07bfc380500b3b11b6bf46e98bccc9
azim <azim.ahmadzadeh@gmail.com>
2022-10-14 11:33:49 -0400

    Cleaned up "pyfunctions.py" and created new scripts for it.

---
+++ utils/data_io.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+
+def dump_data(df: pd.DataFrame, path: str):
+    """
+    stores a given dataframe as a CSV file, with the given filename.
+    
+    :param df: the pandas dataframe to be stored as a CSV file
+    :param path: the path where the data should be stored at
+    
+    :return:
+    """
+    # converting the new pd.dataframe into a readable csv file
+    new_csv_file = df.to_csv(path, index=False)
+    return new_csv_file
+
+
+def load_data(arg1):
+    """
+    @TODO: Please complete this similar to the example above.
+    :param arg1:
+    :return:
+    """
+    # deciding which collumns to add and remove
+    X = arg1.iloc[:, :-1].values
+    # converting the array into a dataset
+    Xdataframe = pd.DataFrame(X)
+    return Xdataframe
+
+
+# @Eugene, please read these comments and remove them afterwards.
+# 1. Stick to one naming convention. E.g., instead of "newcsvfile" use
+# "new_csv_file", and instead of "Xdataframe" use "x_dataframe". This
+# convention is called Snake Case.
+#
+# 2. As you can see in this scrip, PyCharm is not underlining my
+# code/comments. Make sure you follow all good practices that IDE
+# suggests to avoid those underlined pieces. See the yellow bulb next to each
+# underlined piece and follow
+# the suggestions.
---
+++ utils/normalizer.py
@@ -0,0 +1,93 @@
+import pandas as pd
+
+
+def normalize_data(data: pd.DataFrame):
+    """
+    runs a zero-one normalizer on the data. Each column of the data will be
+    normalized separately. To do this, we use the `MinMaxScaler` function of
+    the `sklearn.preprocessing` package.
+    
+    :param data: the dataframe to be normalized
+    :return: the normalized data with the same column names.
+    """
+    from sklearn.preprocessing import MinMaxScaler
+    
+    # fitting the scalar function into my data
+    data_normalized = MinMaxScaler().fit_transform(data)
+    
+    # converting the scaled dataset back into the dataset variable type
+    data_normalized_df: pd.DataFrame = pd.DataFrame(data_normalized)
+    
+    # reading columns back to the new dataframe type variable
+    data_normalized_df.columns = ['TOTUSJH_median', 'TOTBSQ_median',
+                                          'TOTPOT_median', 'TOTUSJZ_median',
+                                          'ABSNJZH_median', 'TOTUSJH_mean',
+                                          'TOTBSQ_mean', 'TOTPOT_mean',
+                                          'TOTUSJZ_mean', 'ABSNJZH_mean',
+                                          'TOTUSJH_min', 'TOTBSQ_min',
+                                          'TOTPOT_min',
+                                          'TOTUSJZ_min', 'ABSNJZH_min',
+                                          'TOTUSJH_max', 'TOTBSQ_max',
+                                          'TOTPOT_max', 'TOTUSJZ_max',
+                                          'ABSNJZH_max',
+                                          'TOTUSJH_std', 'TOTBSQ_std',
+                                          'TOTPOT_std', 'TOTUSJZ_std',
+                                          'ABSNJZH_std', 'TOTUSJH_skewness',
+                                          'TOTBSQ_skewness', 'TOTPOT_skewness',
+                                          'TOTUSJZ_skewness',
+                                          'ABSNJZH_skewness',
+                                          'TOTUSJH_kurtosis',
+                                          'TOTBSQ_kurtosis', 'TOTPOT_kurtosis',
+                                          'TOTUSJZ_kurtosis',
+                                          'ABSNJZH_kurtosis',
+                                          'TOTUSJH_last_value',
+                                          'TOTBSQ_last_value',
+                                          'TOTPOT_last_value',
+                                          'TOTUSJZ_last_value',
+                                          'ABSNJZH_last_value', 'label']
+    return data_normalized_df
+
+
+def centralize_data(data: pd.DataFrame):
+    """
+    @TODO: complete this docstring following the example above.
+    
+    :param data:
+    :return:
+    """
+    from sklearn.preprocessing import StandardScaler
+    
+    # fitting the scalar function into my data
+    data_centralized = StandardScaler().fit_transform(data)
+    
+    # covnerting the scaled dataset back into the dataset variable type
+    data_centralized_df: pd.DataFrame = pd.DataFrame(data_centralized)
+    
+    # readding collums back to the new dataframe type variable
+    data_centralized_df.columns = ['TOTUSJH_median', 'TOTBSQ_median',
+                                        'TOTPOT_median', 'TOTUSJZ_median',
+                                        'ABSNJZH_median', 'TOTUSJH_mean',
+                                        'TOTBSQ_mean', 'TOTPOT_mean',
+                                        'TOTUSJZ_mean', 'ABSNJZH_mean',
+                                        'TOTUSJH_min',
+                                        'TOTBSQ_min', 'TOTPOT_min',
+                                        'TOTUSJZ_min', 'ABSNJZH_min',
+                                        'TOTUSJH_max', 'TOTBSQ_max',
+                                        'TOTPOT_max',
+                                        'TOTUSJZ_max', 'ABSNJZH_max',
+                                        'TOTUSJH_std', 'TOTBSQ_std',
+                                        'TOTPOT_std', 'TOTUSJZ_std',
+                                        'ABSNJZH_std',
+                                        'TOTUSJH_skewness',
+                                        'TOTBSQ_skewness', 'TOTPOT_skewness',
+                                        'TOTUSJZ_skewness', 'ABSNJZH_skewness',
+                                        'TOTUSJH_kurtosis',
+                                        'TOTBSQ_kurtosis', 'TOTPOT_kurtosis',
+                                        'TOTUSJZ_kurtosis', 'ABSNJZH_kurtosis',
+                                        'TOTUSJH_last_value',
+                                        'TOTBSQ_last_value',
+                                        'TOTPOT_last_value',
+                                        'TOTUSJZ_last_value',
+                                        'ABSNJZH_last_value', 'label']
+    return data_centralized_df
+
