31f54476e1e96e4991fcf3137a8b460e0127f45d
eugeny_30 <eugenekang2007@outlook.com>
2022-11-06 20:10:18 -0500

    commit test pls work

--- regressors/P1_P2_minmaxnormal_DTR.py
+++ regressors/P1_P2_minmaxnormal_DTR.py
@@ -7,7 +7,7 @@ from classification import flare_forecasting_dt as ffdt
 from visualisation import visualize_data_stats as vds
 import pandas as pd
 from sklearn.model_selection import train_test_split
-"hit"
+
 ### Import p1 and p2 of SWAN-SF dataset
 p1 = vds.import_csv(r"C:\Users\eugen\Downloads\p1_40sf (1).csv")
 p2 = vds.import_csv(r"C:\Users\eugen\Downloads\p2_40sf (1).csv")
@@ -95,4 +95,3 @@ F1_score_10 = ffdt.return_f1_score(yn, pred_10)
 
 Final_F1 = (F1_score_01 + F1_score_02 + F1_score_03 + F1_score_04 + F1_score_05 + F1_score_06 +
             F1_score_07 + F1_score_08 + F1_score_09 + F1_score_10) / 10
-Print(regressor_10)
